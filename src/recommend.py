6fe1215959f2297fcc8af261358277e7c6a0c17f
Antoine Grelety <Antoinegrelety@MacBook-Pro-de-AntoineG.local>
2020-05-09 10:34:40 +0200

    add requirements

--- src/recommend.py
+++ src/recommend.py
@@ -2,16 +2,16 @@ from src.utils import get_my_recommendations
 from src.args import parse_recommend_arguments
 
 import numpy as np
+import logging
 
 
 if __name__ == "__main__":
 
     parameters = parse_recommend_arguments()
-    print(parameters)
 
     X_predict = np.load(parameters['matrix_path'])
     user_id = parameters['user_id']
 
     df = get_my_recommendations(user_id, X_predict, parameters['books_path'])
 
-    print(f"Top 20 books recommended {df.head(20)}")
+    logging.warning(f"Top 20 books recommended {df.head(20)}")
